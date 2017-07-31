76a6c816bcea1a861eb62db7e5015199d245b516
James Costello <jcostello10@gmail.com>
2017-07-31 13:03:36 -0700

    logging middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,13 @@ exports.logout = (req, res) => {
   req.flash('Success', 'You are now logged out.');
   res.redirect('/');
 }
+
+exports.isLoggedIn = (req, res, next) => {
+  //checking if the user is authenticatied
+  if(req.isAuthenticated()){
+    next();
+    return
+  }
+  req.flash('error', 'Oops you must be logged in to do that!');
+  res.redirect('/login');
+}
